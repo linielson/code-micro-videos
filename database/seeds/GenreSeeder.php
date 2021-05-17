d28ec7852c221b4b494b0f8f6fbe847bcd5be009
Linielson <linielsonrosa@gmail.com>
2021-05-17 08:27:52 -0300

    Fill Category at GenreSeeder

--- database/seeds/GenreSeeder.php
+++ database/seeds/GenreSeeder.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Models\Genre;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class GenreSeeder extends Seeder
 {
     public function run()
     {
-        factory(\App\Models\Genre::class, 10)->create();
+        factory(Genre::class, 10)->create();
+        $categories = Category::all();
+        Genre::all()
+            ->each(function(Genre $genre) use($categories){
+                $categoriesId = $categories->random(5)->pluck('id')->toArray();
+                $genre->categories()->attach($categoriesId);
+            });
     }
 }
